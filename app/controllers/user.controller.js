930b5b7bbebafa9a1fb66fade7c8aae0f47d1e4f
fazaimron27 <cincaumyself@gmail.com>
2022-11-16 12:12:11 +0700

    feat: added user business logic

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,66 @@
+const db = require('../models');
+
+const User = db.user;
+
+exports.show = (req, res) => {
+  const { id } = req.params;
+
+  User.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.status(200).send({
+          error: false,
+          message: 'Data pengguna ditemukan.',
+          data: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            address: data.address,
+            createdAt: data.createdAt,
+            updatedAt: data.updatedAt,
+          },
+        });
+      } else {
+        res.status(404).send({
+          error: true,
+          message: 'Data pengguna tidak ditemukan.',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: true,
+        message:
+          err.message || 'Terjadi kesalahan saat mengambil data pengguna',
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const { id } = req.params;
+
+  User.update(req.body, {
+    where: { id },
+  })
+    .then((result) => {
+      if (result == 1) {
+        res.status(200).send({
+          error: false,
+          message: 'Data pengguna berhasil diperbarui.',
+        });
+      } else {
+        res.status(404).send({
+          error: true,
+          message: 'Tidak dapat memperbarui data pengguna.',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: true,
+        message:
+          err.message || 'Terjadi kesalahan saat memperbarui data pengguna',
+      });
+    });
+};
