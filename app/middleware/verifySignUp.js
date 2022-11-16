6e2088223a26a0fd6972c812b8e0fc65837dbec6
fazaimron27 <cincaumyself@gmail.com>
2022-11-16 12:04:45 +0700

    feat: add a middleware for sign up verification

---
+++ app/middleware/verifySignUp.js
@@ -0,0 +1,44 @@
+const db = require('../models');
+
+const { ROLES } = db;
+
+const User = db.user;
+
+const checkDuplicateEmail = (req, res, next) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({
+        message: 'Gagal! Email sudah digunakan!',
+      });
+      return;
+    }
+
+    next();
+  });
+};
+
+const checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i += 1) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: `Gagal! Peran ${req.body.roles[i]} tidak ada!`,
+        });
+        return;
+      }
+    }
+  }
+
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateEmail,
+  checkRolesExisted,
+};
+
+module.exports = verifySignUp;
