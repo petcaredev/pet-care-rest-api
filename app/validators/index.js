82d462a4051f3d5237aabd576ae1bd72bbfdd938
fazaimron27 <cincaumyself@gmail.com>
2022-11-21 12:12:54 +0700

    feat: export clinic validator

--- app/validators/index.js
+++ app/validators/index.js
@@ -1,7 +1,9 @@
 const authValidator = require('./auth.validator');
 const userValidator = require('./user.validator');
+const clinicValidator = require('./clinic.validator');
 
 module.exports = {
   authValidator,
   userValidator,
+  clinicValidator,
 };
