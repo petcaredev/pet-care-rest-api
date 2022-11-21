fac173fbdaf0a77901ef5186a92122361aee11dd
fazaimron27 <cincaumyself@gmail.com>
2022-11-21 11:01:26 +0700

    feat: init clinic seeder

--- app/seeders/index.js
+++ app/seeders/index.js
@@ -1,9 +1,11 @@
 const role = require('./role.seeder');
 const user = require('./user.seeder');
+const clinic = require('./clinic.seeder');
 
 const init = () => {
   role();
   user();
+  clinic();
 };
 
 module.exports = init;
