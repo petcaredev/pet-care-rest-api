0a4cb4f638a82557719273d0c17251cb0209e5e9
fazaimron27 <cincaumyself@gmail.com>
2022-11-21 10:50:48 +0700

    feat: adding middleware for file upload

---
+++ app/middleware/uploadFile.js
@@ -0,0 +1,35 @@
+const multer = require('multer');
+const path = require('path').resolve('./');
+
+const validateFile = (req, file, cb) => {
+  const allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
+
+  if (allowedMimes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type.'));
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, `${path}/storage/uploads/static/`);
+  },
+  filename: (req, file, cb) => {
+    const ext = file.originalname.substring(
+      file.originalname.lastIndexOf('.'),
+      file.originalname.length,
+    );
+    const fileName = `${Math.random()
+      .toString(36)
+      .substring(2, 15)}${Math.random().toString(36).substring(2, 15)}${ext}`;
+    cb(null, fileName);
+  },
+});
+
+const uploadFile = multer({
+  storage,
+  fileFilter: validateFile,
+});
+
+module.exports = uploadFile;
