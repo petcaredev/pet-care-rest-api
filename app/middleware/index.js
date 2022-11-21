0a0a2236ea6ad596c83faaafce531c45137241e4
fazaimron27 <cincaumyself@gmail.com>
2022-11-21 10:58:13 +0700

    feat: middleware exports uploaded files and middleware to check if the user already has a clinic

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,7 +1,11 @@
 const authJwt = require('./authJwt');
 const verifySignUp = require('./verifySignUp');
+const checkUserHasClinic = require('./userHasClinic');
+const uploadFile = require('./uploadFile');
 
 module.exports = {
   authJwt,
   verifySignUp,
+  checkUserHasClinic,
+  uploadFile,
 };
