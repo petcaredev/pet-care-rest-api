6845df8210925643982429046caa71056284d315
fazaimron27 <cincaumyself@gmail.com>
2022-11-23 14:20:51 +0700

    feat: added some user seeders and clinics as well as the generate image configuration

--- app/seeders/clinic.seeder.js
+++ app/seeders/clinic.seeder.js
@@ -1,16 +1,71 @@
+const fs = require('fs');
+const path = require('path').resolve('./');
 const db = require('../models');
+const generateImage = require('../utils/generateImage.utils');
+
+if (!fs.existsSync(`${path}/storage/uploads/static/`)) {
+  fs.mkdirSync(`${path}/storage/uploads/static/`, { recursive: true });
+  fs.readdirSync(`${path}/storage/uploads/static/`).forEach((file) => {
+    fs.unlinkSync(`${path}/storage/uploads/static/${file}`);
+  });
+}
 
 const Clinic = db.clinic;
 
 const init = async () => {
   Clinic.create({
+    id: 1,
     name: 'Lilipoet Pet Clinic',
     address:
-      'Jl. Bhayangkara No.30A, Ngupasan, Kec. Gondomanan, Kota Yogyakarta, Daerah Istimewa Yogyakarta ',
-    posterPath: 'zh6z257skvggkw7sp8v5zm.jpeg',
+      'Jl. Bhayangkara No.30A, Ngupasan, Kec. Gondomanan, Kota Yogyakarta, Daerah Istimewa Yogyakarta',
+    posterPath: generateImage(),
     phone: '087839281900',
   }).then((clinic) => {
     clinic.setUser([2]);
+  });
+
+  Clinic.create({
+    id: 2,
+    name: 'Sunshine Pet Vet',
+    address:
+      'Jl. Monjali No.25, Nandan, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
+    posterPath: generateImage(),
+    phone: '0817269116',
+  }).then((clinic) => {
+    clinic.setUser([3]);
+  });
+
+  Clinic.create({
+    id: 3,
+    name: 'Bravo Vet & Groom',
+    address:
+      'Jl. Tasura No.68, Krodan, Maguwoharjo, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
+    posterPath: generateImage(),
+    phone: '08562811162',
+  }).then((clinic) => {
+    clinic.setUser([4]);
+  });
+
+  Clinic.create({
+    id: 4,
+    name: 'Omah Vets & Petshop',
+    address:
+      'Jl. Bung Tarjo No.7, Baciro, Kec. Gondokusuman, Kota Yogyakarta, Daerah Istimewa Yogyakarta',
+    posterPath: generateImage(),
+    phone: '081999906419',
+  }).then((clinic) => {
+    clinic.setUser([5]);
+  });
+
+  Clinic.create({
+    id: 5,
+    name: "Dji'O Pet Care + Vet",
+    address:
+      'Gg. Menur No.15, Santren, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
+    posterPath: generateImage(),
+    phone: '085640473600',
+  }).then((clinic) => {
+    clinic.setUser([6]);
   });
 };
 
--- app/seeders/user.seeder.js
+++ app/seeders/user.seeder.js
@@ -6,9 +6,9 @@ const User = db.user;
 const init = async () => {
   User.create({
     id: 1,
-    name: 'Pengguna',
-    email: 'pengguna@mail.com',
-    password: bcrypt.hashSync('pengguna', 8),
+    name: 'Pet Care User',
+    email: 'petcareuser@mail.com',
+    password: bcrypt.hashSync('petcareuser', 8),
     phone: '1234567890',
     address: 'Jl. Pengguna',
   }).then((user) => {
@@ -17,11 +17,60 @@ const init = async () => {
 
   User.create({
     id: 2,
-    name: 'Klinik',
-    email: 'klinik@mail.com',
-    password: bcrypt.hashSync('klinik', 8),
-    phone: '1234567890',
-    address: 'Jl. Klinik',
+    name: 'Lilipoet Pet Clinic',
+    email: 'lilipoetpet@mail.com',
+    password: bcrypt.hashSync('lilipoetpet', 8),
+    phone: '087839281900',
+    address:
+      'Jl. Bhayangkara No.30A, Ngupasan, Kec. Gondomanan, Kota Yogyakarta, Daerah Istimewa Yogyakarta',
+  }).then((user) => {
+    user.setRoles([2]);
+  });
+
+  User.create({
+    id: 3,
+    name: 'Sunshine Pet Vet',
+    email: 'sunshinepetvet@mail.com',
+    password: bcrypt.hashSync('sunshinepetvet', 8),
+    phone: '0817269116',
+    address:
+      'Jl. Monjali No.25, Nandan, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
+  }).then((user) => {
+    user.setRoles([2]);
+  });
+
+  User.create({
+    id: 4,
+    name: 'Bravo Vet & Groom',
+    email: 'bravovetgroom@mail.com',
+    password: bcrypt.hashSync('bravovetgroom', 8),
+    phone: '08562811162',
+    address:
+      'Jl. Tasura No.68, Krodan, Maguwoharjo, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
+  }).then((user) => {
+    user.setRoles([2]);
+  });
+
+  User.create({
+    id: 5,
+    name: 'Omah Vets & Petshop',
+    email: 'omahvetspetshop@mail.com',
+    password: bcrypt.hashSync('omahvetspetshop', 8),
+    phone: '081999906419',
+    address:
+      'Jl. Bung Tarjo No.7, Baciro, Kec. Gondokusuman, Kota Yogyakarta, Daerah Istimewa Yogyakarta',
+  }).then((user) => {
+    user.setRoles([2]);
+  });
+
+  User.create({
+    id: 6,
+    name: "Dji'O Pet Care + Vet",
+    email: 'djiopetcare@mail.com',
+    password: bcrypt.hashSync('djiopetcare', 8),
+    phone: '085640473600',
+    address:
+      'Gg. Menur No.15, Santren, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta',
   }).then((user) => {
     user.setRoles([2]);
   });
