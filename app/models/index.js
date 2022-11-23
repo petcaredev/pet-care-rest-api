1513ad7bc618c758de18de8a622ff69ca1f15117
fazaimron27 <cincaumyself@gmail.com>
2022-11-23 17:29:50 +0700

    feat: adding sequelize port configuration

--- app/models/index.js
+++ app/models/index.js
@@ -3,6 +3,7 @@ const dbConfig = require('../config/db.config');
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
+  port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
 
