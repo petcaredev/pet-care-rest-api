118c72bcb50f1854fb938e484b5050f9e26ee289
fazaimron27 <cincaumyself@gmail.com>
2022-11-29 14:06:21 +0700

    feat: implement service validation form

--- app/validators/index.js
+++ app/validators/index.js
@@ -1,9 +1,11 @@
 const authValidator = require('./auth.validator');
 const userValidator = require('./user.validator');
 const clinicValidator = require('./clinic.validator');
+const serviceValidator = require('./service.validator');
 
 module.exports = {
   authValidator,
   userValidator,
   clinicValidator,
+  serviceValidator,
 };
