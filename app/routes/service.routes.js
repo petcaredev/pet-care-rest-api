741583b203f9c3c10110fd7e7be42816b433c129
fazaimron27 <cincaumyself@gmail.com>
2022-11-29 14:03:44 +0700

    feat: added a service routes

---
+++ app/routes/service.routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { authJwt } = require('../middleware');
+const { serviceValidator } = require('../validators');
+const controller = require('../controllers/service.controller');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Authorization, Origin, Content-Type, Accept',
+    );
+    next();
+  });
+
+  router.get(
+    '/clinic/:clinicId',
+    [authJwt.verifyToken, authJwt.isClinic],
+    controller.index,
+  );
+  router.post(
+    '/',
+    [authJwt.verifyToken, authJwt.isClinic],
+    serviceValidator.store,
+    controller.store,
+  );
+  router.get('/:id', [authJwt.verifyToken, authJwt.isClinic], controller.show);
+  router.put(
+    '/:id',
+    [authJwt.verifyToken, authJwt.isClinic],
+    serviceValidator.update,
+    controller.update,
+  );
+  router.delete(
+    '/:id',
+    [authJwt.verifyToken, authJwt.isClinic],
+    controller.destroy,
+  );
+
+  app.use('/api/services', router);
+};
