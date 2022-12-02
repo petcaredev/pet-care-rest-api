bebf8784b53ac17e230ba9734724b691eb5b8930
fazaimron27 <cincaumyself@gmail.com>
2022-12-02 19:12:04 +0700

    feat: extend jwt token expiration

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -59,7 +59,7 @@ exports.signin = (req, res) => {
       }
 
       const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
+        expiresIn: 604800, // 7 days
       });
 
       const authorities = [];
