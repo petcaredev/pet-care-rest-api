81da13188aaa4e1085be3f6a5af89ca2ced9c452
fazaimron27 <cincaumyself@gmail.com>
2022-12-08 21:03:03 +0700

    feat: export reservation validation form

--- app/validators/index.js
+++ app/validators/index.js
@@ -2,10 +2,12 @@ const authValidator = require('./auth.validator');
 const userValidator = require('./user.validator');
 const clinicValidator = require('./clinic.validator');
 const serviceValidator = require('./service.validator');
+const reservationValidator = require('./reservation.validator');
 
 module.exports = {
   authValidator,
   userValidator,
   clinicValidator,
   serviceValidator,
+  reservationValidator,
 };
