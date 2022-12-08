979242c209f1e17814d79222d10cc3755a57c075
fazaimron27 <cincaumyself@gmail.com>
2022-12-08 21:04:40 +0700

    feat: adding business logic to reservations

---
+++ app/controllers/reservation.controller.js
@@ -0,0 +1,140 @@
+const db = require('../models');
+
+const Reservation = db.reservation;
+const Service = db.service;
+
+const User = db.user;
+const Clinic = db.clinic;
+
+exports.store = async (req, res) => {
+  const { userId, clinicId, date, petName, petType, description, services } =
+    req.body;
+
+  const user = await User.findByPk(userId);
+  const clinic = await Clinic.findByPk(clinicId);
+
+  const reservation = {
+    date,
+    petName,
+    petType,
+    description,
+    status: false,
+  };
+
+  const reservationData = await Reservation.create(reservation);
+
+  reservationData.setUser(user);
+  reservationData.setClinic(clinic);
+
+  const serviceData = await Service.findAll({
+    where: {
+      id: services,
+    },
+
+    attributes: ['id', 'name', 'price'],
+  });
+
+  reservationData.setServices(serviceData);
+
+  res.status(201).send({
+    error: false,
+    message: 'Data reservasi berhasil ditambahkan.',
+    data: {
+      id: reservationData.id,
+      date: reservationData.date,
+      petName: reservationData.petName,
+      petType: reservationData.petType,
+      description: reservationData.description,
+      status: reservationData.status,
+      createdAt: reservationData.createdAt,
+      updatedAt: reservationData.updatedAt,
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      clinic: {
+        name: clinic.name,
+        address: clinic.address,
+        phone: clinic.phone,
+      },
+      services: serviceData.map((service) => ({
+        id: service.id,
+        name: service.name,
+        price: service.price,
+      })),
+    },
+  });
+};
+
+exports.show = async (req, res) => {
+  const { id } = req.params;
+
+  const reservation = await Reservation.findByPk(id, {
+    attributes: [
+      'id',
+      'date',
+      'petName',
+      'petType',
+      'description',
+      'status',
+      'createdAt',
+      'updatedAt',
+    ],
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'name', 'email', 'phone', 'address'],
+      },
+      {
+        model: Clinic,
+        as: 'clinic',
+        attributes: ['id', 'name', 'address', 'phone'],
+      },
+      {
+        model: Service,
+        as: 'services',
+        attributes: ['id', 'name', 'price'],
+      },
+    ],
+  });
+
+  if (!reservation) {
+    return res.status(404).send({
+      error: true,
+      message: 'Data reservasi tidak ditemukan.',
+    });
+  }
+
+  res.status(200).send({
+    error: false,
+    message: 'Data reservasi berhasil ditemukan.',
+    data: reservation,
+  });
+};
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+
+  const reservation = await Reservation.findByPk(id);
+
+  if (!reservation) {
+    return res.status(404).send({
+      error: true,
+      message: 'Data reservasi tidak ditemukan.',
+    });
+  }
+
+  const status = true;
+
+  await reservation.update({
+    status,
+  });
+
+  res.status(200).send({
+    error: false,
+    message: 'Reservasi telah selesai.',
+  });
+};
